b4ddd2dd95869722df126eb15a4d29ebbbf2afc0
Jacob Mortensen <jacob.mortensen.it@gmail.com>
2017-05-05 20:05:27 +0200

    Remaining of chapter 4

--- src/main/scala/Chapter4.scala
+++ src/main/scala/Chapter4.scala
@@ -1,6 +1,6 @@
-object Chapter4 {
+object `Chapter 4` {
 
-    object Exercise_4_3 {
+    object `Exercise 4.3` {
         def Try[A](a: => A) : Option[A] = {
             try Some(a)
             catch {case e : Exception => None }
@@ -37,7 +37,7 @@ object Chapter4 {
         }
     }
 
-    object Exercise_4_4 {
+    object `Exercise 4.5 and 4.6` {
 
         def sequence[A](a: List[Option[A]]) : Option[List[A]] = {
             val init : Option[List[A]] = Some(List())
@@ -63,4 +63,59 @@ object Chapter4 {
         traverse(a)(Predef.identity)
       }
     }
+
+    object `Exercise 4.6 and 4.7` {
+      trait EEither[+E, +A]{
+        def map[B](f: A => B) : EEither[E, B] = {
+          this match {
+            case Error(e) => Error(e)
+            case Success(a) => Success(f(a))
+          }
+        }
+
+        def flatMap[EE >: E, B](f: A => EEither[EE, B]) : EEither[EE, B] = {
+          this match {
+            case Error(e) => Error(e)
+            case Success(a) => f(a)
+          }
+        }
+
+        def orElse[EE >: E, B >: A](b: EEither[EE, B]) : EEither[EE, B] = {
+          this match {
+            case Error(_) => b
+            case Success(a) => Success(a)
+          }
+        }
+
+        def map2[EE >: E, B, C](value : EEither[EE, B])(f: (A, B) => C) : EEither[EE, C] = {
+          this.flatMap(a => value.flatMap(b => Success(f(a,b))))
+        }
+
+        def sequence[E, A](es:List[EEither[E, A]]) : EEither[E, List[A]] = {
+          traverse(es)(Predef.identity)
+        }
+
+        def traverse[E, A, B](as: List[A])(f: A => EEither[E, B]) : EEither[E, List[B]] = {
+          val init : EEither[E, List[B]] = Success(List())
+          as.foldRight(init)((item: A, acc : EEither[E, List[B]]) => {
+            acc.flatMap(list => f(item).flatMap(it => Success(it :: list)))
+          })
+        }
+
+        def traverseWithMap2[E, A, B](as: List[A])(f: A => EEither[E, B]) : EEither[E, List[B]] = {
+          as match {
+            case Nil => Success(Nil)
+            case h :: t => {
+              val head = f(h)
+              val tail = traverseWithMap2(t)(f)
+              head.map2(tail)(_ :: _)
+            }
+          }
+        }
+      }
+      case class Error[E, A](error: E) extends EEither[E, A]
+      case class Success[E, A](value: A) extends EEither[E, A]
+    }
+
+
 }
